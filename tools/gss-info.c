d9810ef30ec69c9ed550da58c4ef0fa9053c7c07
David Schleef <david.schleef@rd.io>
2013-07-03 11:19:39 -0700

    gss-info: convert from isom-parse-test

--- tools/gss-info.c
+++ tools/gss-info.c
@@ -1,5 +1,6 @@
 
 #include <gst-streaming-server/gss-isom.h>
+#include <gst-streaming-server/gss-isom.h>
 
 #include <stdio.h>
 
@@ -38,6 +39,8 @@ main (int argc, char *argv[])
 
   for (i = 1; i < argc; i++) {
     GssIsomFile *file;
+    GssIsomTrack *video_track;
+    GssIsomTrack *audio_track;
     gboolean ret;
 
     file = gss_isom_file_new ();
@@ -47,7 +50,10 @@ main (int argc, char *argv[])
       g_print ("parse failed");
     }
 
-    gss_isom_file_fragmentize (file);
+    video_track = gss_isom_movie_get_video_track (file->movie);
+    audio_track = gss_isom_movie_get_audio_track (file->movie);
+    g_print ("%s %d %d\n",
+        argv[i], video_track->esds.avg_bitrate, audio_track->esds.avg_bitrate);
 
     gss_isom_file_free (file);
   }
